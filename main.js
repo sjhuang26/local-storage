e6e97d79ef798ae8d54c936c6ae267567bbf6632
sjhuang26 <sjhuang26@gmail.com>
2017-07-01 16:09:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,22 @@
     return document.getElementById(id);
   }
   
-  var $editorItems = $("editorItems");
-  var $editorSave = 
+  function $new(s) {
+    var div = document.createElement('div');
+    div.innerHTML = s;
+    return div.firstChild;
+  }
+  
+  var $addItem = $("editor__add-item");
+  var $items = $("editor__items");
+  var $removeItem = $("editor__remove-item");
+  var $save = $("editor__save");
+  
+  var ITEM_HTML = '<div><input type="text" /><input type="text" /></div>';
+  
+  function addItem() {
+    $save.appendChild($new(ITEM_HTML));
+  }
+  
+  $addItem.addEventListener("click", addItem);
 }());
